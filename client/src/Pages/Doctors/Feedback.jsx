488b3e1e0b0f5bf4cf0ba0761f57af07ad20bbee
spikerrbg <val_05@abv.bg>
2023-11-14 11:41:42 +0200

    edit feedback

--- client/src/Pages/Doctors/Feedback.jsx
+++ client/src/Pages/Doctors/Feedback.jsx
@@ -1,46 +1,72 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
 // import { reviews } from "../../../data/reviews.js";
-import Reviews from "./Reviews.jsx";
+// import Reviews from "./Reviews.jsx";
 import FeedBackForm from "./FeedBackForm.jsx";
 import * as reviewsService from "../../serviceR/reviewsService.js";
 
-const Feedback = ({ doctor,id }) => {
+const Feedback = ({ doctor }) => {
   const [feedbackForm, setFeedbackForm] = useState(false);
-  const [reviews, setReviews] = useState([])
-  useEffect(() => {
+  const [reviews, setReviews] = useState([]);
+  const { id, totalRating } = doctor;
 
-    reviewsService.getAll()
-    .then((result) => {
-      setReviews(result);
-      
-    })
-    .catch((err) => console.log(err));
-}, []);
+  useEffect(() => {
+    // Fetch reviews for the specific doctor
+    reviewsService.getAll(id)
+      .then((result) => {
+        setReviews(result);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
 
   return (
     <div>
-      <div className="mb-[50px]">
-        <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px] ">
-          All review ({doctor.totalRating})
-        </h4>
-
-        {reviews.map((items, id) => (
-          <Reviews key={id} items={items} />
-        ))}
-      </div>
-      <div>
-        {!feedbackForm && (
-          <div className="text-center">
-            <button onClick={() => setFeedbackForm(true)} className="btn">
-              Give Feedback{" "}
-            </button>{" "}
-          </div>
-        )}
-
-        {feedbackForm && <FeedBackForm />}
-      </div>
+    <div className="mb-[50px]">
+      <h4 className="text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]">
+        All review ({doctor.totalRating})
+      </h4>
     </div>
+    
+      {reviews.map((items) => (
+        <div key={items.id} className="flex justify-between gap-10 mb-[30px]">
+          <div className="flex gap-3">
+            <figure className="w-10 h-10 rounded-full">
+              <img className="w-full" src={items.photo} alt="" />
+            </figure>
+            <div>
+              <h5 className="text-[16px] leading-6 text-primaryColor font-bold">
+                {items.name}
+              </h5>
+              <h5 className="text-[16px] leading-6 text-primaryColor font-bold">
+                {items.username}
+              </h5>
+              <p>{items.reviewtext}</p>
+              <p className="text-[14px] leading-6 text-textColor">{items.date}</p>
+              <p className="text_para mt-3 font-medium text-[15px]">
+                {items.reviews}
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-1">
+            {/* {[...Array(items.rating).keys()].map((_, index) => (
+              <AiFillStar key={index} color="#0067FF" />
+            ))} */}
+            ({items.rating})
+          </div>
+        </div>
+      ))}
+      {feedbackForm && <FeedBackForm  doctor={doctor} doctorId={id}/>}
+      <div>
+      {!feedbackForm && (
+        <div className="text-center">
+          <button onClick={() => setFeedbackForm(true)} className="btn">
+            Give Feedback
+          </button>
+        </div>
+      )}
+    </div>
+  </div>
   );
 };
 
