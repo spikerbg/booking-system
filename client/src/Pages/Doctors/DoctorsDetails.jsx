6f382e6bb685406dfa7c2c76964168c9f8d43b00
spikerrbg <val_05@abv.bg>
2023-11-14 11:43:01 +0200

    edit doctor details

--- client/src/Pages/Doctors/DoctorsDetails.jsx
+++ client/src/Pages/Doctors/DoctorsDetails.jsx
@@ -1,107 +1,82 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import Testimonial from "../../Components/Testimonial/Testimonial";
+import { doctors } from ".././../../data/doctors.js";
+import DoctorCard from "./DoctorCard";
 import * as doctorService from "../../serviceR/doctorService";
-import starIcon from "../../assets/images/Star.png";
-import DoctorAbout from "./DoctorAbout.jsx";
-import Feedback from "./Feedback.jsx";
-import SidePanel from "./SidePanel.jsx";
-
-const DoctorsDetails = ({}) => {
-  const [doctor, setDoctor] = useState(null);
-  const [tab, setTab] = useState("about");
-  const { id } = useParams();
-
+import { useState, useEffect } from "react";
+const Doctors = () => {
+ const [doctors, setDoctors] = useState([])
+ const [searchQuery, setSearchQuery] = useState("");
+  const [filteredDoctors, setFilteredDoctors] = useState([]);
+  
   useEffect(() => {
-    async function fetchDoctor() {
-      try {
-        const selectedDoctor = await doctorService.getOne(id);
-        setDoctor(selectedDoctor);
-      } catch (error) {
-        // Handle error, e.g., display an error message
-        console.error("Error fetching doctor data:", error);
-      }
-    }
 
-    fetchDoctor();
-  }, [id]);
+    doctorService.getAll()
+    .then((result) => {
+      setDoctors(result);
+      setFilteredDoctors(result); // Initialize filtered doctors with all doctors
+    })
+    .catch((err) => console.log(err));
+}, []);
 
-  if (!doctor) {
-    return <div>Loading...</div>; // Return a loading indicator if data is not yet available
-  }
+const handleSearchChange = (e) => {
+  const query = e.target.value;
+  setSearchQuery(query);
 
-  const {
-    name,
-    specialty,
-    avgRating,
-    totalRating,
-    photo,
-    short,
-  } = doctor;
+  // Filter doctors based on the search query
+  const filtered = doctors.filter((doctor) =>
+    doctor.name.toLowerCase().includes(query.toLowerCase())
+  );
+  setFilteredDoctors(filtered);
+};
 
   return (
-    <section>
-      <div className="max-w-[1170px] px-5 mx-auto">
-        <div className="grid md:grid-cols-3 gap-[50px]">
-          <div className="md:col-span-2">
-            <div className="flex items-center gap-5">
-              <figure>
-                <img
-                  src={photo}
-                  alt={name}
-                  className="w-[200px] h-[200px] rounded"
-                />
-              </figure>
-              <div>
-                <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
-                  {specialty}
-                </span>
-                <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
-                  {name}
-                </h3>
-                <div className="flex items-center gap-[6px]">
-                  <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
-                    <img src={starIcon} alt="star" /> {avgRating}
-                  </span>
-                  <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
-                    ({totalRating})
-                  </span>
-                </div>
-                <p className="text_para text-[14px] leading-5 md:text-[15px] lg:max-w-[390px]">
-                  {short}
-                </p>
-              </div>
-            </div>
-
-            <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
-              <button
-                onClick={() => setTab("about")}
-                className={`${
-                  tab === "about" && `border-b border-solid border-primaryColor`
-                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
-              >
-                About
+    <>
+      <section className="bg-[#fff9ea]">
+        <div className="container text-center">
+          <div>
+            <h2 className="heading">Find a Doctors</h2>
+            <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex item-center justify-between">
+              <input
+                type="search"
+                placeholder="Search Doctor"
+                className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+              <button className="btn mt-0 rounded-[0px] rounded-r-md">
+                Search
               </button>
-              <button
-                onClick={() => setTab("feedback")}
-                className={`${
-                  tab === "feedback" &&
-                  `border-b border-solid border-primaryColor`
-                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
-              >
-                Feedback
-              </button>
-            </div>
-            <div className="mt-[50px]">
-              {tab === "about" && <DoctorAbout doctor={doctor} />}
-
-              {tab === "feedback" && <Feedback doctor={doctor} doctorId={id} />}
             </div>
           </div>
-          <SidePanel />
         </div>
-      </div>
-    </section>
+      </section>
+      <section>
+        <div className="container">
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5
+          "
+          >
+            {filteredDoctors.map((doctor,id) => (
+              <DoctorCard key={id} doctor={doctor} doctorId={doctor.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our paient say</h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our health system offers unmatched,
+              expert health care
+            </p>
+          </div>
+          <Testimonial />
+        </div>
+      </section>
+    </>
   );
 };
 
-export default DoctorsDetails;
+export default Doctors;
