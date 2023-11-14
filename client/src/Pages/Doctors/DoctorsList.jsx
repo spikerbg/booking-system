c0101f61e84c55ff4a4cd882c1474c7b459ca48d
spikerrbg <val_05@abv.bg>
2023-11-14 11:42:09 +0200

    edit doctor list

--- client/src/Pages/Doctors/DoctorsList.jsx
+++ client/src/Pages/Doctors/DoctorsList.jsx
@@ -1,10 +1,22 @@
-import { doctors } from "../../../data/doctors.js";
+// import { doctors } from "../../../data/doctors.js";
+import { useState, useEffect } from "react";
 import DoctorCard from "./DoctorCard.jsx";
+import * as doctorService from "../../serviceR/doctorService.js";
+
+
 const DoctorsList = () => {
+  const [doctors, setDoctors] = useState([])
+  useEffect(() => {
+
+    doctorService.getAll()
+        .then(result => setDoctors(result))
+        .catch(result => console.log(result))
+}, []);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-      {doctors.map((doctor, index) => (
-        <DoctorCard doctor={doctor} key={index} />
+      {doctors.map((doctor, id) => (
+        <DoctorCard doctor={doctor} key={id} />
       ))}
     </div>
   );
