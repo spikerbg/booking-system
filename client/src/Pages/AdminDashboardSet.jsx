f6cc737d8d5a611e6bddf88e150274b927b9d09f
spikerrbg <val_05@abv.bg>
2023-11-23 04:23:51 +0200

    Update AdminDashboardSet.jsx

--- client/src/Pages/AdminDashboardSet.jsx
+++ client/src/Pages/AdminDashboardSet.jsx
@@ -143,7 +143,7 @@ export default function AdminDashboardSet(){
             <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
             <p className="mt-2 text-sm text-gray-700">
               A list of all the users in your account including their name, title, email and role.
-              {email}
+              {email}{createdAt}
             </p>
           </div>
           <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
